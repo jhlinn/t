2d2bc8124aad9c163904fe61458d19bdedb4adb5
cho <cho.kaewya@gmail.com>
2022-08-14 14:05:24 +0200

    adds .gz on file creation

--- gdrive/app.py
+++ gdrive/app.py
@@ -4,13 +4,13 @@ from pydrive.drive import GoogleDrive
 
 import tarfile
 
-# tar = tarfile.open("./downloaded_files.gz", "w:gz")
-# tar.add("/downloads/complete",)
-# tar.close()
+tar = tarfile.open("./downloaded_files.gz", "w:gz")
+tar.add("/downloads/complete",)
+tar.close()
 
 gauth = GoogleAuth()
 drive = GoogleDrive(gauth)
 
-file2 = drive.CreateFile({'title':'downloaded_files'})
+file2 = drive.CreateFile({'title':'downloaded_files.gz'})
 file2.SetContentFile('./downloaded_files.gz')
 file2.Upload()
