9d8275ffdae3a2332a85e5f4973d7078859537a3
cho <cho.kaewya@gmail.com>
2022-08-14 13:00:13 +0200

    supports gdrive

---
+++ gdrive/app.py
@@ -0,0 +1,16 @@
+# import gzip
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+import tarfile
+
+tar = tarfile.open("./downloaded_files.gz", "w:gz")
+tar.add("/downloads/complete",)
+tar.close()
+
+gauth = GoogleAuth()
+drive = GoogleDrive(gauth)
+
+file2 = drive.CreateFile({'title':'downloaded_files'})
+file2.SetContentFile('./downloaded_files.gz')
+file2.Upload()
